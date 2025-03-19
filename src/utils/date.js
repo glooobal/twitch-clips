6abf48f3ede163b945e40d1aefceb666862601ea
Jakub <jakubgorzny@icloud.com>
2025-03-19 11:09:53 +0100

    Deleted unused functions

--- src/utils/date.js
+++ src/utils/date.js
@@ -9,22 +9,3 @@ export const getYesterdayTimeRange = () => {
         end: endTime.toISOString(),
     };
 };
-
-export const getLastWeekTimeRange = () => {
-    const now = new Date();
-
-    const daysSinceMonday = (now.getUTCDay() + 6) % 7;
-
-    const lastMonday = new Date(
-        Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - daysSinceMonday - 7, 4, 0, 0)
-    );
-
-    const thisMonday = new Date(
-        Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - daysSinceMonday, 4, 0, 0)
-    );
-
-    return {
-        start: lastMonday.toISOString(),
-        end: thisMonday.toISOString(),
-    };
-};
